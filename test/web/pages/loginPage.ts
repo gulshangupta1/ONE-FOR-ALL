33b62b5754379a54479a5c67cbeb2f18fc8cf862
gulshangupta003 <gulshangupta003@gmail.com>
2024-04-05 16:29:35 +0530

    Integrated for web automation

---
+++ test/web/pages/loginPage.ts
@@ -0,0 +1,19 @@
+import { BasePage } from "./base/basePage";
+
+export class LoginPage extends BasePage {
+    constructor() {
+        super();
+    }
+
+    private locators = {
+        userNameInputField: "//input[@name='username']",
+        passwordInputField: "//input[@name='password']",
+        submitButton: "//button[@type='submit']"
+    };
+
+    async login(username: string, password: string): Promise<void> {
+        await this.setValue(this.locators.userNameInputField, username);
+        await this.setValue(this.locators.passwordInputField, password);
+        await this.click(this.locators.submitButton);
+    }
+}
